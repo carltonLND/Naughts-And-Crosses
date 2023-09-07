f19f36069c3d4cfe8069563c3d6e1eeee9622b1d
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-07 17:14:44 +0100

    Add redux store

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import App from "./components/App"
+import { themeConfig } from "./utils/themeConfig"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ChakraProvider theme={themeConfig}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>,
+)
