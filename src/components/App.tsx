53a0dc5ed3c69ae216968107698720260ad4b800
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-07 17:15:13 +0100

    Add App component

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import { Flex } from "@chakra-ui/react"
+import Game from "../features/game/Game"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <Flex
+      height="100vh"
+      width="100vw"
+      justifyContent="space-between"
+      direction="column"
+    >
+      <Game />
+      <Footer />
+    </Flex>
+  )
+}
+
+export default App
