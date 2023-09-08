df21d5f272852a4746033ceea2d369384e686397
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-08 15:01:07 +0100

    Remove strict mode whilst testing sockets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
+// import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
@@ -7,11 +7,11 @@ import App from "./components/App"
 import { themeConfig } from "./utils/themeConfig"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider theme={themeConfig}>
-        <App />
-      </ChakraProvider>
-    </Provider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <ChakraProvider theme={themeConfig}>
+      <App />
+    </ChakraProvider>
+  </Provider>,
+  // </React.StrictMode>,
 )
