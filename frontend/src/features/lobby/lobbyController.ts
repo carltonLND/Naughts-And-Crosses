dd6e3efc66e4fb32b2bbae02d87d830e9b1c6074
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-08 15:01:18 +0100

    Rename file

--- frontend/src/features/lobby/lobbyController.ts
+++ frontend/src/features/lobby/lobbyController.ts
@@ -1,23 +1,25 @@
 import { Socket } from "socket.io-client"
 import { AppDispatch } from "../../app/store"
-import { OnlinePlayer, addClient, removeClient, setLobbyId } from "./lobbySlice"
+import {
+  OnlinePlayer,
+  addClient,
+  removeClient,
+  setClientId,
+  setLobbyId,
+} from "./lobbySlice"
 
-export function setupLobbyController(socket: Socket, dispatch: AppDispatch) {
+export function setupLobbyListeners(socket: Socket, dispatch: AppDispatch) {
   socket.on("lobby created", (lobbyId: string, player: OnlinePlayer) => {
-    console.log("lobby created", lobbyId)
     dispatch(setLobbyId(lobbyId))
     dispatch(addClient(player))
+    dispatch(setClientId(player.id))
   })
 
   socket.on("player joined", (player: OnlinePlayer) => {
-    console.log("new player", player.id)
     dispatch(addClient(player))
   })
 
   socket.on("player left", (playerId: string) => {
-    console.log("player left")
     dispatch(removeClient(playerId))
   })
-
-  socket.emit("new lobby", "Player One")
 }
